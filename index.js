564b38b5f74a263723a5bb20c605a4b94ed8e016
HolsSchool <HolsSchool@Holly-Cabacungans-MacBook-Air.local>
2016-07-11 19:17:31 -1000

    complete assignment

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//http://jsbin.com/pupazuyano/edit?js,console
-
 /* create variables */
 
 /**
@@ -7,48 +5,47 @@
 * Variables with a String as their value
 *
 * Declare variables named `firstName`, `lastName`, and `birthPlace`.
-* Fill out the values for these with your own data or make it up. It should contain a String
+* Fill out the values for these with your own data or make it up.
+*
+* It should be a String.
 **/
+var firstName = "Holly";
+console.log(firstName);
+var lastName = "Cabacungan";
+console.log(lastName);
+var birthPlace = "Wailuku";
+console.log(birthPlace)
 
-var firstName = "Samuel";
-var lastName = "Jaxson";
-var birthPlace = "hollywood";
 
-console.log('firstName:', firstName);
-console.log('lastName:', lastName);
-console.log('birthPlace:',  birthPlace);
+
+
+
 /**
 * #2
 * Variables with a Number as their value
 *
-* Declare variables named `favoriteNumber`, `currentYear`, and `thatOnePrinceSong`.
+* Declare variables named `favoriteNumber`, `currentYear`,
+* and `thatOnePrinceSong`.
+*
 * Their values should be a Number
 **/
-
-var favoriteNumber = 8;
+var favoriteNumber = 7;
+console.log(favoriteNumber);
 var currentYear = 2016;
-var thatOnePrinceSong = 1999;
+console.log(currentYear);
 
-console.log('favoriteNumber: ' + favoriteNumber);
-console.log('currentYear: ' + currentYear);
-console.log('thatOnePrinceSong: ' + thatOnePrinceSong);
+
 
 /**
 * #3
 * Variables with Boolean values
 *
-* Declare variables named `isDaytime`, `isLeftHanded`, `inHawaii`, and `isHappyCoding`
+* Declare variables named `isDaytime`, `isLeftHanded`, `inHawaii`,
+* and `isHappyCoding`
+*
 * Their values should be a Boolean
 **/
 
-var isDaytime = false;
-var isLeftHanded = false;
-var inHawaii = true;
-var isHappyCoding = true;
-
-console.log('isDayTime: ' + isDaytime);
-console.log('isLeftHanded: ' + isLeftHanded);
-console.log('isHappyCoding: ' + isHappyCoding);
 
 
 /**
@@ -56,108 +53,83 @@ console.log('isHappyCoding: ' + isHappyCoding);
 * Variables with null
 *
 * Declare variables named `enrolledAtDevLeague` and `completedFridayPrep`
+*
 * Their values should be a Null value
 **/
 
-var enrolledAtDevLeague = null;
-var completedFridayPrep = null;
 
-console.log('enrolledAtDevLeague: ' + enrolledAtDevLeague);
-console.log('completedFridayPrep: ' + completedFridayPrep);
+
 /**
 * #5a
 * Variables with an Array for it's value. This Array will contain:
 *
-* Declare a variable named `westCoast` which is an Array. This Array contains the names of all the states
+* Declare a variable named `westCoast` which is an Array.
+* This Array contains the names of all the states
 * which can be found along the west coast of The United States.
 **/
 
-var westCoast = ["Oregon", "Washington", "California"];
-
-console.log('wesssstsiide: ' + westCoast);
 
 
 /**
 * #5b
 * Variables with an Array for it's value. This Array will contain:
 *
-* Declare a variable named `evenNumbers` which is an Array. This Array contains numbers that
-* are considered 'even values'. Have at least ten even values in this array.
+* Declare a variable named `evenNumbers` which is an Array.
+* This Array contains numbers that are considered 'even values'.
+* Have at least ten even values in this array.
 **/
 
-var evenNumbers = [2, 4, 6, 8, 10];
 
-console.log('evenNumbers: ' + evenNumbers);
 
 /**
 * #6a Arithmetic
 * Variables with the outcome of an arithmetic operation:
-* 
-* Declare a variable named `sumOfNumbers` which is a Number. This Number contains the result of performing an arithmetic
-* operation on two numbers. Have this variable store the result of adding 3 numbers of your choosing.
 *
-* Extend this behavior to include subtraction, multiplication and division. 
+* Declare a variable named `sumOfNumbers` which is a Number. This Number
+* contains the result of performing an arithmetic operation on two numbers.
+* Have this variable store the result of adding 3 numbers of your choosing.
+*
+* Extend this behavior to include subtraction, multiplication and division.
 */
 
-var sumOfNumbers = 1 + 2 + 3;
-var differenceOfNumbers = 3 - 2- 1;
-var productOfNumbers = 2*4*6;
-var quotentOfNumbers = (10 / 5) / 2; 
 
-console.log('sumOfNumbers 1 + 2 + 3 = ' + sumOfNumbers);
-console.log('differenceOfNumbers 3 - 2- 1 = ' + differenceOfNumbers);
-console.log('productOfNumbers 2 * 4 * 6 = ' + productOfNumbers);
-console.log('quotentOfNumbers = (10/5) / 2 = ' + quotentOfNumbers);
+
 
 /**
 * #6b Comparisons & Logic
 * Variables with the outcome of a comparison operation:
 *
-* Declare a variable named 'isEqual' which is a Boolean. This Boolean contains the result of performing a comparison to
-* test if two strings are equal. We want to compare the two strings "Tacocat" and "tacocat" and console.log() the result.
+* Declare a variable named `isEqual` which is a Boolean. This Boolean
+* contains the result of performing a comparison to test if two strings
+* are equal. We want to compare the two strings "Tacocat" and "tacocat" and
+* `console.log()` the result.
 *
-* BONUS: See if you can come up with a few examples of double comparison operator(==) vs triple comparison operator(===)
+* NOTE:
+*   There is no test which tests your console.log()'s output'.
+*   Also, there are zero tests for the BONUS below.
+*
+* BONUS:
+*   See if you can come up with a few examples of double comparison
+*   operator(==) vs triple comparison operator(===).
 */
 
-var isEqual = "Tacocat" == "tacocat";
-console.log('isEqual: ' + isEqual); //false
-
-var isSame = 5 == "5";
-console.log('isSame: ' + isSame); //true
-
-var isSame = 5 === "5";
-console.log('isSame: ' + isSame); //false
 
 
 /**
 * #6c Assignments
 * Variables with the outcome of an assignment operation:
 *
-* Declare a variable named generalContainer. This variable will be used to store multiple types of
-* values as we `assign` them to show how variables in JavaScript are not bound to a single data type
+* Declare a variable named `generalContainer`. This variable will be used to store multiple types of
+* values as we assign them to show how variables in JavaScript are not bound to a single data type
 * and can change values.
 *
-* Write 5 different assignment statements and `console.log()` the value of the variable after each 
+* Write 5 different assignment statements and `console.log()` the value of the variable after each
 * assignment statement to show the current value.
+*
+* NOTE:
+*   The tests for this section cannot test your variable as it changes,
+*   it can only test that you declared the variable by giving it the right name.
 */
-
-var generalContainer = 7; //assignment number
-console.log('generalContainer ' + generalContainer); //7
-
-generalContainer = generalContainer += 1;
-console.log('generalContainer += 1 = ' + generalContainer); //8 
-
-generalContainer = generalContainer -= 2;
-console.log('generalContainer -= 2 = ' + generalContainer); // 6
-
-generalContainer = generalContainer *= 3;
-console.log('generalContainer *= 3 = ' + generalContainer); //18
-
-generalContainer = generalContainer /= 6;
-console.log('generalContainer /= 6 = ' + generalContainer); //3
-
-generalContainer = generalContainer %= 2;
-console.log('generalContainer %= 2 = ' + generalContainer); //1
 
 
 
@@ -167,23 +139,12 @@ console.log('generalContainer %= 2 = ' + generalContainer); //1
 *
 * Declare a Function named `jump` which takes a single argument, `height`
 *
-* This function should return a String along the lines of, "You Jumped 9 feet high!"
+* This function should return a String, "You jumped 9 feet high!"
 * if invoked in this way -> jump(9);
 *
-* Store the return value to a variable and use console.log to inspect the value which was passed back by your function
+* Store the return value to a variable named `jumpResult` and use console.log to inspect the value which was returned by your function
 **/
 
-function jump(height){
-  /*console.log("You jumped " + height + " feet high!");*/
-  return "You jumped " + height + " feet high!";
-}
-
-
-jump();
-/*jump();*/
-
-var hops = jump(11);
-console.log(hops);
 
 
 /**
@@ -196,33 +157,3 @@ console.log(hops);
 * if invoked in this way -> cook("Tomatoes", "Cheese", "Pizza");
 **/
 
-function cook(ingredient1, ingredient2, recipeName){
-  return ingredient1 + " and " + ingredient2 + " make a " + recipeName + "!";
-}
-
-/*cook("tomatoes", "cheese", "pizza");*/
-
-var dinner = cook("natto", "uni", "pizza");
-console.log(dinner);
-
-
-
-
-
-
-
-
-
-
-/**
-* #7c
-* Declare Functions
-*
-* declare Function named
-* Their values should be a Number
-**/
-/*
-function sum(n1, n2){
-  return n1 + n2;
-}
-console.log(sum(1, 2));*/
